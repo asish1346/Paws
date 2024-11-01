e9443128e15e7363b49cd1e6417bd71e8d90153d
Ashu1346 <hhu04054@gmail.com>
2024-11-02 05:07:26 +0530

    Added Wallet Connection

---
+++ bot/config/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+
+    REF_LINK: str = "https://t.me/PAWSOG_bot/PAWS?startapp=mrG2me3a"
+
+    AUTO_TASK: bool = True
+    AUTO_CONNECT_WALLET: bool = False
+
+    DELAY_EACH_ACCOUNT: list[int] = [20, 30]
+    IGNORE_TASKS: list[str] = ["telegram"]
+    ADVANCED_ANTI_DETECTION: bool = True
+
+    USE_PROXY_FROM_FILE: bool = False
+
+
+settings = Settings()
+
